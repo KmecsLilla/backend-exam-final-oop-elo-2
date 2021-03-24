91a1282dd11a6a28d0b1b03903f4de07427761bf
KmecsLilla <kmecs.lilla@gmail.com>
2021-03-24 16:28:18 +0100

    Implement OOP-task

--- src/main/java/com/codecool/farm/Farm.java
+++ src/main/java/com/codecool/farm/Farm.java
@@ -1,5 +1,47 @@
 package com.codecool.farm;
 
+import com.codecool.farm.animal.Animal;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Farm {
 
+    List<Animal> animals = new ArrayList<>();
+
+    public Farm(List<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void feedAnimals() {
+        for (Animal animal: animals) {
+            animal.feed();
+        }
+    }
+
+    public void butcher(Butcher butcher) {
+        List<Animal> animalsReadyToDie = new ArrayList<>();
+        for (Animal animal: animals) {
+            if (butcher.canButcher(animal)) {
+                animalsReadyToDie.add(animal);
+            }
+        }
+        animals.removeAll(animalsReadyToDie);
+    }
+
+    public boolean isEmpty() {
+        return animals.isEmpty();
+    }
+
+    public List<String> getStatus() {
+        List<String> result = new ArrayList<>();
+        for (Animal animal: animals) {
+            result.add(animal.toString());
+        }
+        return result;
+    }
 }
--- src/main/java/com/codecool/farm/animal/Animal.java
+++ src/main/java/com/codecool/farm/animal/Animal.java
@@ -2,4 +2,17 @@ package com.codecool.farm.animal;
 
 public abstract class Animal {
 
+    protected int size = 0;
+
+    public int getSize() {
+        return size;
+    }
+
+    public abstract void feed();
+
+    @Override
+    public String toString() {
+        return "There is a " + size +
+            " sized " + getClass().getSimpleName().toLowerCase() + " in the farm.";
+    }
 }
